b092b5ac45e4f266a328fcc1a994d781721b6395
heryndra86 <mher0029@student.monash.edu>
2026-08-17 18:38:16 +1000

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
 window.JOB_TRACKER_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://tjpakenprtpmcrbielwf.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqcGFrZW5wcnRwbWNyYmllbHdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NDQ3NjYsImV4cCI6MjEwMjUyMDc2Nn0.9IL5IzSA1Ex52LMwa-gTK7mqAin_0Q6O-A9lfusIhjo",
 };
